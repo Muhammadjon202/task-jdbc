c7266f2f55fd507f308a4cc19c15ed975397cabf
Hamidov Muhammadjon <muhammadjon202>
2022-04-25 16:20:08 +0500

    update db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,8 @@ public class Main {
         String firstName = scanner.nextLine();
         System.out.println("Enter Your Last Name: ");
         String lastName = scanner.nextLine();
+        System.out.println("Enter Your Class Name: ");
+        String class_name = scanner.nextLine();
         System.out.println("Enter Your Gender: ");
         String gender = scanner.nextLine();
         System.out.println("Enter Your Phone Number: ");
@@ -18,18 +20,33 @@ public class Main {
         scanner.nextLine();
         try {
             Connection connection = DriverManager.getConnection(jdbcUrl, username, pass);
-//            Statement statement = connection.createStatement();
-//            String sql = "insert into students_info(first_name, last_name, gender, phone_num)" +
-//                    "values('" + firstName + "', '" + lastName + "', '" + gender + "', " + phoneNum + ")";
-//            statement.executeUpdate(sql);
-            PreparedStatement stmt = connection.prepareStatement("insert into students_info values(?,?,?,?)");
+            PreparedStatement stmt = connection.prepareStatement("insert into students_info values(?,?,?,?,?)");
             stmt.setString(1, firstName);
             stmt.setString(2, lastName);
-            stmt.setString(3, gender);
-            stmt.setLong(4, phoneNum);
+            stmt.setString(3, class_name);
+            stmt.setString(4, gender);
+            stmt.setLong(5, phoneNum);
+            PreparedStatement statement = connection.prepareStatement("insert into student_subjects values(?)");
+            int a = 0;
+            while(a < 1) {
+                System.out.println("1. Set Subject Info");
+                System.out.println("2. Stop");
+                int btn = scanner.nextInt();
+                scanner.nextLine();
+                if (btn == 1) {
+                    System.out.println("Enter Your Subject: ");
+                    String setSubject = scanner.nextLine();
+                    statement.setString(1, setSubject);
+                } else if (btn == 2) {
+                    a = 2;
+                } else {
+                    System.out.println("Incorrect");
+                }
+            }
         } catch (SQLException e) {
             System.out.println("Error Connection");
             e.printStackTrace();
         }
+
     }
 }
